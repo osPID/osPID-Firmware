88981a1cc90d915edf26e0d042a2a1c1082c323f
Benjamin K. Stuhl <stuhl@umd.edu>
2013-04-05 16:24:06 -0400

    add a no-erase EEPROM write method

--- osPID_Firmware/ospSettingsHelper.h
+++ osPID_Firmware/ospSettingsHelper.h
@@ -51,7 +51,7 @@ public:
 
   int crcValue() const { return crc16; }
 
-  template<typename T> static void eepromRead(int address, T& value) {
+  template<typename T> static void eepromRead(unsigned int address, T& value) {
     byte *p = (byte *)&value;
 
     for (byte n = sizeof(T); n; n--) {
@@ -61,12 +61,50 @@ public:
     }
   }
 
-  template<typename T> static void eepromWrite(int address, const T& value) {
+  template<typename T> static void eepromWrite(unsigned int address, const T& value) {
     const byte *p = (const byte *)&value;
 
     for (byte n = sizeof(T); n; n--) {
       if (eeprom_read_byte((byte *)address) != *p)
         eeprom_write_byte((byte *)address, *p);
+      p++;
+      address++;
+    }
+  }
+
+  // this function uses inline assembler because there is no function in avr-libc
+  // which provides the required functionality
+  // it is basically a clone of the implementation of eeprom_write_byte, except that
+  // we program EEPM1 to 1 to set program-only mode (rather than erase-then-program
+  // mode, which is the default of 0)
+  template<typename T> static void eepromClearBits(unsigned int address, const T& value) {
+    const byte *p = (const byte *)&value;
+
+    for (byte n = sizeof(T); n; n--) {
+      __asm__ __volatile__ (
+        "1: sbic %0, %1\n" // wait for EEPROM write to complete
+        "   rjmp 1b\n"
+        "   out %0, __zero_reg__\n" // clear EECR
+        "   out %2, %3\n" // set EEPROM address
+        "   out %4, %5\n"
+        "   out %6, %7\n" // set EEPROM data
+        "   sbi %0, %8\n" // set program-only mode
+        "   in __tmp_reg__, %9\n" // save flags
+        "   cli\n"                // block interrupts
+        "   sbi %0, %10\n" // set EEPROM Master Write Enable
+        "   sbi %0, %11\n" // trigger write by setting EEPROM Write Enable
+        "   out %9, __temp_reg__\n" // restore flags (re-enable interrupts)
+        : // no register writes
+        : "I" (_SFR_IO_ADDR(EECR)), "I" (EEWE),
+          "I" (_SFR_IO_ADDR(EEARH)), "r" (address >> 8),
+          "I" (_SFR_IO_ADDR(EEARL)), "r" (address & 0xFF),
+          "I" (_SFR_IO_ADDR(EEDR)), "r" (*p),
+          "I" (EEPM1),
+          "I" (SREG),
+          "I" (EEMWE),
+          "I" (EEWE)
+        : // no clobbers
+      );
       p++;
       address++;
     }
